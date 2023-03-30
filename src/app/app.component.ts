f0b48bf09fe7fab64fc562e4dc6b3051f9adfa3a
Mariano <marianoovelar200@gmail.com>
2023-03-30 12:22:34 -0300

    ej 01 y 02 terminado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-app';
+
+  numero1: number = 1;
+  numero2: number = 2;
+  resultado: number = 0;
+  sumar() {
+    this.resultado = Number(this.numero1) + Number(this.numero2);
+  }
+
+
+  edad1: number = 0;
+  edad2: number = 0;
+  promedio: number = 0;
+  sumaEdad: number = 0;
+  calcular() {
+    if (Number(this.edad1) >= 0 && Number(this.edad1) <= 100 && Number(this.edad2) >= 0 && Number(this.edad2) <= 100)
+      this.promedio = (Number(this.edad1) + Number(this.edad2)) / 2;
+      this.sumarEdad();
+  }
+  limpiar() {
+    this.edad1 = 0;
+    this.edad2 = 0;
+    this.promedio = 0;
+    this.sumaEdad = 0;
+  }
+  sumarEdad() {
+    this.sumaEdad = Number(this.edad1) + Number(this.edad2);
+  }
+
 }
