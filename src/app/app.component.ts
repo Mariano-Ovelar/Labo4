2a48d0927af76fce42950b8a26ba9c64ea746bc2
Mariano <marianoovelar200@gmail.com>
2023-04-02 20:09:04 -0300

    corrigiendo el codigo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,31 +8,31 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'my-app';
 
-  numero1: number = 1;
-  numero2: number = 2;
+  numero1: number = 0;
+  numero2: number = 0;
   resultado: number = 0;
+
   sumar() {
     this.resultado = Number(this.numero1) + Number(this.numero2);
   }
-
 
   edad1: number = 0;
   edad2: number = 0;
   promedio: number = 0;
   sumaEdad: number = 0;
+
   calcular() {
-    if (Number(this.edad1) >= 0 && Number(this.edad1) <= 100 && Number(this.edad2) >= 0 && Number(this.edad2) <= 100)
+    if (Number(this.edad1) >= 0 && Number(this.edad1) <= 100 && Number(this.edad2) >= 0 && Number(this.edad2) <= 100) {
       this.promedio = (Number(this.edad1) + Number(this.edad2)) / 2;
-      this.sumarEdad();
+      this.sumaEdad = Number(this.edad1) + Number(this.edad2);
+    }
   }
+
   limpiar() {
     this.edad1 = 0;
     this.edad2 = 0;
     this.promedio = 0;
     this.sumaEdad = 0;
-  }
-  sumarEdad() {
-    this.sumaEdad = Number(this.edad1) + Number(this.edad2);
   }
 
 }
